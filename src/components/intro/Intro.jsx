495568279bace058be35cdf9c21e7d69cf13bc20
mattty-t <mattty.tran@gmail.com>
2021-11-15 19:55:41 +1000

    creating portfolio page + styling

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -22,7 +22,7 @@ export default function Intro() {
       <div className="right">
         <div className="wrapper">
           <h2>Hi there, I am </h2>
-          <h1>Matthew</h1>
+          <h1>Matt</h1>
           <h3>
             <span ref={textRef}></span>
           </h3>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,40 @@
 import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
 
 export default function Portfolio() {
+  const list = [
+    { id: "website", title: "Website" },
+    {
+      id: "ruby terminal app",
+      title: "Ruby Terminal Application",
+    },
+    {
+      id: "ruby marketplace app",
+      title: "Ruby Marketplace App",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
-      sadsads
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList title={item.title} />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="=" alt="" />
+          <h3>Website Portfolio</h3>
+        </div>
+        <div className="item">
+          <img src="assets/website_portfolio" alt="" />
+          <h3>Ruby Terminal Application</h3>
+        </div>
+        <div className="item">
+          <img src="=" alt="" />
+          <h3>Two sided Marketplace</h3>
+        </div>
+      </div>
     </div>
   );
 }
